60dcca7bae1168c5d53d1e6b9b9c1ef9bd9f7470
Lojueusainsusbe <l.dekker.4@umail.leidenuniv.nl>
2020-03-11 15:00:10 +0100

    script geschreven dat Nsequencer.rs omschrijft naar goede Nsequencer.rs

---
+++ treo/nseqscript.py
@@ -0,0 +1,41 @@
+# script om de file om te schrijven
+import re
+
+rs = open("Nsequencer.rs", "r")
+
+rslines = rs.readlines()
+
+index = rslines.index("    let mem_init = MemInitial::default()\n") + 1
+numbers = re.findall(r'\d+', rslines[index])
+
+Tword = "T" + str(numbers[0])
+mword = "m" + str(numbers[1])
+
+rslines.remove('use reo_rs::building::{*, NameDef::*};\n')
+rslines.remove("use reo_rs::{Instruction::*, Term::*};\n")
+
+rslines.insert(7, "#[allow(unused_imports)]\n")
+rslines.insert(8, "use reo_rs::{\n")
+rslines.insert(9, "    building::{NameDef::*, *},\n")
+rslines.insert(10, "    Instruction::*,\n")
+rslines.insert(11, "    Term::*,\n")
+rslines.insert(12, "};\n")
+
+rslines[14] = "pub extern \"C\" fn reors_generated_proto_create() -> CProtoHandle {\n"
+
+line = "    " + Tword + ": 'static + Send + Sync + Sized,\n"
+line2 = "    " + Tword + ": 'static + Send + Sync + Sized + core::str::FromStr,\n"
+rslines[rslines.index(line)] = line2
+
+
+line = "        .with<" + Tword + ">(\"" + mword +"\", \"\"0\"\".into());\n"
+line2 = "        .with(\"" + mword + "\", " + Tword + "::from_str_expect(\"0\"));\n"
+rslines[rslines.index(line)] = line2
+
+rs.close()
+
+rsnew = open("Nsequencer.rs", "w")
+
+rsnew.writelines(rslines)
+
+rsnew.close()
