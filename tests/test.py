13df7e1fd0bef70d6e12b5b03ef9c99b724a8208
Lojueusainsusbe <l.dekker.4@umail.leidenuniv.nl>
2020-03-15 13:50:38 +0000

    struggelen op de server

---
+++ tests/test.py
@@ -0,0 +1,73 @@
+import sys
+import subprocess
+from argparse import ArgumentParser
+
+parser = ArgumentParser ()
+parser.add_argument("file", type=str, help="name of file to write results to")
+args = parser.parse_args()
+filename = args.file
+
+runs=25
+actions=1 #0000
+sizes=[4] #,16,64,256,512,1024]
+
+protocols=["alt", "eao", "ear", "seq"]
+
+def det_producer (prot, size):
+    if prot == "alt" or prot == "seq":
+        return size
+    return 1
+
+def det_consumer (prot, size):
+    if prot == "seq":
+        return 0
+    if prot == "alt":
+        return 1
+    return size
+
+def det_productions (prot, size, actions):
+    if prot == "eao":
+        return actions*size
+    return actions
+
+def det_consumptions (prot, size, actions):
+    if prot == "seq":
+        return 0
+    if prot == "alt":
+        return actions*size
+    return actions
+
+def parse_output (time):
+    time = time[:-2] # remove endline
+    return float(time)
+
+results = open(filename + ".txt", "w")
+
+for prot in protocols:
+    for size in sizes:
+        ctime = 0
+        ttime = 0
+        numproducers = str(det_producer(prot, size))
+        numconsumers = str(det_consumer(prot, size))
+        productions = str(det_productions(prot, size, actions))
+        consumptions = str(det_consumptions(prot, size, actions))
+
+        results.write(prot + " " + numproducers + " " + productions + " " + numconsumers + " " + consumptions)
+
+        for run in range(runs):
+            # C
+            time = subprocess.check_output(['./../c/test', prot, numproducers, productions, numconsumers, consumptions])
+
+            ctime += parse_output(time)
+            # Treo
+            time = subprocess.check_output(['./../treo/test', prot, numproducers, productions, numconsumers, consumptions])
+
+            ttime += parse_output(time)
+
+        ctime = ctime / runs
+        ttime = ttime / runs
+
+        results.write("average C time over " + str(runs) + " runs: " + str(ctime))
+        results.write("average treo time over " + str(runs) + " runs: " + str(ttime))
+
+results.close()
